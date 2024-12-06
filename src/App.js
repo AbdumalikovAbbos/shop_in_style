5961274710be9ece3bf1a59fef386406ef5373c7
Abbos Abdumalikov <abbosabdumalikov960@gmail.com>
2024-12-06 19:31:20 +0500

    shop_react

---
+++ src/App.js
@@ -0,0 +1,250 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container px-4 px-lg-5">
+          <a className="navbar-brand" href="#!">
+            Start Bootstrap
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  Shop
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header className="bg-dark text-white py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="text-center">
+            <h1 className="display-4 fw-bolder">Shop in Style</h1>
+            <p className="lead fw-normal text-white-50 mb-0">
+              With this shop homepage template
+            </p>
+          </div>
+        </div>
+      </header>
+
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 mt-5">
+          <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Fancy Product</h5>
+                    <span>$40.00 - $80.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x3 25"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                <img
+                  className="card-img-top"
+                  src="https://via.placeholder.com/500x325"
+                  alt="Product"
+                />
+                <div className="card-body p-4">
+                  <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    <span>$20.00</span>
+                  </div>
+                </div>
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                  <div className="text-center">
+                    <a className="btn btn-outline-dark mt-auto" href="#!">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="py-5 bg-dark">
+        <div className="container px-4 px-lg-5">
+          <div className=" text-center text-white">
+            Copyright © Your Website 2023
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
